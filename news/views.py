02d23c08479ec72fa7f9194cc8d17aca53c0d9d3
Harikrishnan K <harikrishnanmaster@gmail.com>
2020-02-11 15:14:56 +0530

    updated

--- news/views.py
+++ news/views.py
@@ -266,12 +266,104 @@ def english_login(request):
 
    return render(request, "news/home_english.html", context)
 def news_list(request):
-    # headlines = Headline.objects.all()[::-1]
-    # context = {
-    #     'object_list': headlines,
-    # }
+    headline_existing = Headline.objects.all()
+    if headline_existing:
+        print('db not empty')
 
-    return render(request, "news/home_english.html")
+    else:
+        print('empty')
+        session = requests.Session()
+        session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
+        url = "https://indianexpress.com/"
+        content = session.get(url, verify=False).content
+        soup = BSoup(content, "html.parser")
+        News = soup.find_all('div', {"class": "other-article"})
+
+        for artcile in News:
+            main = artcile.find_all('a')[0]
+            link = main['href']
+            print(link)
+            image_src = str(main.find('img')['src'])
+            print(image_src)
+            title = artcile.find('h3').text
+            print(title)
+            date_english = date.today()
+            # print(date_english)
+            new_headline = Headline()
+            new_headline.title = title
+            new_headline.url = link
+            new_headline.language = 1
+            new_headline.category = 2
+            new_headline.image = image_src
+            # new_headline.date = date_english
+            new_headline.save()
+    # date_english = date.today()
+    # b=headline_existing.date
+    # print(b)
+    for c in headline_existing:
+        date_english = date.today()
+        a = c.date
+        print(a)
+        print('----')
+        print(date_english)
+        if a != date_english:
+
+            session = requests.Session()
+            session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
+            url = "https://indianexpress.com/"
+            content = session.get(url, verify=False).content
+            soup = BSoup(content, "html.parser")
+            News = soup.find_all('div', {"class": "other-article"})
+
+            for artcile in News:
+                main = artcile.find_all('a')[0]
+                link = main['href']
+                print(link)
+                image_src = str(main.find('img')['src'])
+                print(image_src)
+                title = artcile.find('h3').text
+                print(title)
+                date_english = date.today()
+                # print(date_english)
+                new_headline = Headline()
+                new_headline.title = title
+                new_headline.url = link
+                new_headline.image = image_src
+                new_headline.language = 1
+                new_headline.category = 2
+                # new_headline.date = date_english
+                new_headline.save()
+            session = requests.Session()
+            session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
+            url_india = "https://indianexpress.com/section/india/"
+            content = session.get(url_india, verify=False).content
+            soup = BSoup(content, "html.parser")
+
+            News = soup.find_all('div', {"class": "articles"})
+            for artcile in News:
+                main = artcile.find_all('a')[0]
+                link = main['href']
+                #     print(link)
+                images = main.find_all('img', {'src': re.compile('.jpg')})
+                for image in images:
+                    #         print(image['src']+'\n')
+                    image_src = image['src']
+                    print(image_src)
+                title = artcile.find('p').text
+                print(title)
+                new_headline = Headline()
+                new_headline.title = title
+                new_headline.url = link
+                new_headline.language = 1
+                new_headline.category = 2
+                new_headline.image = image_src
+                new_headline.save()
+    headlines = Headline.objects.all()[::-1]
+    context = {
+        'object_list': headlines,
+    }
+
+    return render(request, "news/home_english.html", context)
 def account(request):
     return render(request,"news/register.html")
 def register(request):
