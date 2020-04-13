684809a9900b757f105e328f7e0644010c22823d
harikrishnan <harikrishnanmaster@GMAIL.COM>
2020-04-14 02:51:56 +0530

    upated

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf.urls import  url
 from django.views.generic import TemplateView
-from news.views import scrape, news_list,scrape_malayalam, login, account, register, logout, malayalam_login,english_login,english_india, malayalam_india_login
+from news.views import scrape, news_list,scrape_malayalam, login, account, register, logout, malayalam_login,english_login,english_india, malayalam_india_login,search
 urlpatterns = [
   path('', scrape, name="scrape"),
   path('newslist', news_list, name="home"),
@@ -14,6 +14,7 @@ urlpatterns = [
   path('malayalam_india_login', malayalam_india_login, name="malayalam_india_login"),
   path('english_login', english_login, name="english_login"),
   path('english_india', english_india, name="english_india"),
+  path('search', search, name="search"),
 
   
 ]
--- news/views.py
+++ news/views.py
@@ -15,15 +15,18 @@ from news.models import Users
 requests.packages.urllib3.disable_warnings()
 import time
 def scrape(request):
+    date_english = date.today()
     headline_existing = Headline.objects.all()
+    check_date=Headline.objects.filter(date=date_english).exists()
+    print(check_date)
     if headline_existing:
-        for c in headline_existing:
-            date_english = date.today()
-            a = c.date
-            print(a)
-            print('----')
-            print(date_english)
-            if a != date_english:
+        if not  check_date:
+            # date_english = date.today()
+            # a = c.date
+            # print(a)
+            # print('----')
+            # print(date_english)
+            # if a != date_english:
                 session = requests.Session()
                 session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
                 url = "https://indianexpress.com/section/cities/"
@@ -55,6 +58,7 @@ def scrape(request):
 
         print('db not empty')
 
+        return news_list(request)
     else:
         print('empty')
         session = requests.Session()
@@ -207,8 +211,10 @@ def login(request):
 def malayalam_login(request):
 
    headlines = Headline.objects.all()[::-1]
+   date_today = date.today()
    context = {
         'object_list': headlines,
+       'date_today': date_today,
     }
 
 
@@ -216,8 +222,10 @@ def malayalam_login(request):
 def malayalam_india_login(request):
 
    headlines = Headline.objects.all()[::-1]
+   date_today = date.today()
    context = {
         'object_list': headlines,
+       'date_today': date_today,
     }
 
 
@@ -228,210 +236,29 @@ def english_login(request):
 
 
    headlines = Headline.objects.all()[::-1]
+   date_today = date.today()
    context = {
         'object_list': headlines,
+        'date_today': date_today,
     }
 
 
    return render(request, "news/home_english.html", context)
 def news_list(request):
-    # headline_existing = Headline.objects.all()
-    # if headline_existing:
-    #     print('db not empty')
-    #
-    # else:
-    #     print('empty')
-    #     session = requests.Session()
-    #     session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
-    #     url = "https://indianexpress.com/"
-    #     content = session.get(url, verify=False).content
-    #     soup = BSoup(content, "html.parser")
-    #     News = soup.find_all('div', {"class": "other-article"})
-    #
-    #     for artcile in News:
-    #         main = artcile.find_all('a')[0]
-    #         link = main['href']
-    #         print(link)
-    #         image_src = str(main.find('img')['src'])
-    #         print(image_src)
-    #         title = artcile.find('h3').text
-    #         print(title)
-    #         date_english = date.today()
-    #         # print(date_english)
-    #         new_headline = Headline()
-    #         new_headline.title = title
-    #         new_headline.url = link
-    #         new_headline.language = 1
-    #         new_headline.category = 1
-    #         new_headline.image = image_src
-    #         # new_headline.date = date_english
-    #         new_headline.save()
-    #     session = requests.Session()
-    #     session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
-    #     url_india = "https://indianexpress.com/section/india/"
-    #     content = session.get(url_india, verify=False).content
-    #     soup = BSoup(content, "html.parser")
-    #
-    #     News = soup.find_all('div', {"class": "articles"})
-    #     for artcile in News:
-    #         main = artcile.find_all('a')[0]
-    #         link = main['href']
-    #         #     print(link)
-    #         images = main.find_all('img', {'src': re.compile('.jpg')})
-    #         for image in images:
-    #             #         print(image['src']+'\n')
-    #             image_src = image['src']
-    #             print(image_src)
-    #         title = artcile.find('p').text
-    #         print(title)
-    #         new_headline = Headline()
-    #         new_headline.title = title
-    #         new_headline.url = link
-    #         new_headline.language = 1
-    #         new_headline.category = 2
-    #         new_headline.image = image_src
-    #         new_headline.save()
-    #     session = requests.Session()
-    #     session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
-    #     url = "https://www.manoramaonline.com/news.html"
-    #     content = session.get(url, verify=False).content
-    #     soup = BSoup(content, "html.parser")
-    #     News_malayalam = soup.find_all('div', {"class": "image-block"})
-    #     for artcile in News_malayalam:
-    #         try:
-    #             main = artcile.find('a')
-    #         except:
-    #             pass
-    #         try:
-    #             link = main['href']
-    #
-    #         except:
-    #             pass
-    #
-    #         try:
-    #             image_src = str(main.find('img')['src'])
-    #
-    #         except:
-    #             pass
-    #
-    #         try:
-    #             title = main['title']
-    #
-    #         except:
-    #             pass
-    #
-    #         new_headline = Headline()
-    #         new_headline.title = title
-    #         new_headline.url = link
-    #         new_headline.language = 2
-    #         new_headline.category = 1
-    #         new_headline.image = image_src
-    #         new_headline.save()
-    # # date_english = date.today()
-    # # b=headline_existing.date
-    # # print(b)
-    # for c in headline_existing:
-    #     date_english = date.today()
-    #     a = c.date
-    #     print(a)
-    #     print('----')
-    #     print(date_english)
-    #     if a != date_english:
-    #
-    #         session = requests.Session()
-    #         session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
-    #         url = "https://indianexpress.com/"
-    #         content = session.get(url, verify=False).content
-    #         soup = BSoup(content, "html.parser")
-    #         News = soup.find_all('div', {"class": "other-article"})
-    #
-    #         for artcile in News:
-    #             main = artcile.find_all('a')[0]
-    #             link = main['href']
-    #             print(link)
-    #             image_src = str(main.find('img')['src'])
-    #             print(image_src)
-    #             title = artcile.find('h3').text
-    #             print(title)
-    #             date_english = date.today()
-    #             # print(date_english)
-    #             new_headline = Headline()
-    #             new_headline.title = title
-    #             new_headline.url = link
-    #             new_headline.image = image_src
-    #             new_headline.language = 1
-    #             new_headline.category = 1
-    #             # new_headline.date = date_english
-    #             new_headline.save()
-    #         session = requests.Session()
-    #         session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
-    #         url_india = "https://indianexpress.com/section/india/"
-    #         content = session.get(url_india, verify=False).content
-    #         soup = BSoup(content, "html.parser")
-    #
-    #         News = soup.find_all('div', {"class": "articles"})
-    #         for artcile in News:
-    #             main = artcile.find_all('a')[0]
-    #             link = main['href']
-    #             #     print(link)
-    #             images = main.find_all('img', {'src': re.compile('.jpg')})
-    #             for image in images:
-    #                 #         print(image['src']+'\n')
-    #                 image_src = image['src']
-    #                 print(image_src)
-    #             title = artcile.find('p').text
-    #             print(title)
-    #             new_headline = Headline()
-    #             new_headline.title = title
-    #             new_headline.url = link
-    #             new_headline.language = 1
-    #             new_headline.category = 2
-    #             new_headline.image = image_src
-    #             new_headline.save()
-    #         session = requests.Session()
-    #         session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
-    #         url = "https://www.manoramaonline.com/news.html"
-    #         content = session.get(url, verify=False).content
-    #         soup = BSoup(content, "html.parser")
-    #         News = soup.find_all('div', {"class": "image-block"})
-    #         for artcile in News:
-    #             try:
-    #                 main = artcile.find('a')
-    #             except:
-    #                 pass
-    #             try:
-    #                 link = main['href']
-    #
-    #             except:
-    #                 pass
-    #
-    #             try:
-    #                 image_src = str(main.find('img')['src'])
-    #
-    #             except:
-    #                 pass
-    #
-    #             try:
-    #                 title = main['title']
-    #
-    #             except:
-    #                 pass
-    #
-    #             new_headline = Headline()
-    #             new_headline.title = title
-    #             new_headline.url = link
-    #             new_headline.language = 2
-    #             new_headline.category = 1
-    #             new_headline.image = image_src
-    #             new_headline.save()
+
     headlines = Headline.objects.all()[::-1]
+    date_today = date.today()
     context = {
         'object_list': headlines,
+        'date_today': date_today,
+
     }
 
     return render(request, "news/home_english.html", context)
 def account(request):
     return render(request,"news/register.html")
+def search(request):
+    return render(request,"news/search.html")
 def register(request):
     name = "wellcomes"
     commentid = "password"
@@ -476,7 +303,10 @@ def english_india(request):
     #         pass
 
     headlines = Headline.objects.all()[::-1]
+    date_today = date.today()
     context = {
         'object_list': headlines,
+        'date_today': date_today,
+
     }
     return render(request,"news/english_india.html",context)
